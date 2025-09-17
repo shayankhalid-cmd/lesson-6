1db37d32b4fc222bfc34a82b891b9c1fd4aac1d2
shayankhalid-cmd <shayan.khalid@renacademy.org>
2025-09-16 18:09:00 -0600

    Add files via upload

---
+++ avg.py
@@ -0,0 +1,23 @@
+a = int(input("enter a value"))
+b = int(input("enter a value"))
+c = int(input("enter a value"))
+
+avg = (a + b + c)/3
+print ("avg = ",avg)
+
+if avg > a and avg > b and avg > c:
+    print("%d is higher than %d,%d" %(avg,a,b,c))
+elif  avg > a and avg > b :
+    print("%d is higher than %d,%d" %(avg,a,b))
+elif  avg > a and avg > c:
+    print("%d is higher than %d,%d" %(avg,a,c))
+elif  avg > b and avg > c :
+    print("%d is higher than %d,%d" %(avg,b,c))
+elif  avg > a :
+    print("%d is higher than %d,%d" %(avg,a))
+elif  avg > b:
+    print("%d is higher than %d,%d" %(avg,b))
+elif  avg > c:
+    print("%d is higher than %d,%d" %(avg,c))
+else:
+    print("invalid input")
