f101f0a76cd9cec04685b989b83541c912a9bbb5
shayankhalid-cmd <shayan.khalid@renacademy.org>
2025-09-16 20:33:29 -0600

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,5 @@
+ch = input(" Enter a Character : ")
+if((ch >= 'a' and ch <= 'z') or (ch >= 'A' and ch <= 'Z')):
+    print("The Given Character ", ch, "is an Alphabet")
+else:
+    print("The Given Character ", ch, "is Not an Alphabet")
