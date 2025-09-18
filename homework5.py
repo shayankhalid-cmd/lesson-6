6ef0a080a5fdcb6edd2598b6c88c90b5d684dc73
shayankhalid-cmd <shayan.khalid@renacademy.org>
2025-09-17 22:04:19 -0600

    Add files via upload

--- homework5.py
+++ homework5.py
@@ -2,4 +2,5 @@ age = int(input("enter your age:"))
 if (age > 10) and (age < 20):
     print ("you are allowed")
 else:
-    print ("you are not allowed")
+    if (age > 20):
+        print ("you are not allowed")
