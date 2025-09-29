8c2bef47884f6666f0b428f839e4a716e73bf0c2
shayankhalid-cmd <shayan.khalid@renacademy.org>
2025-09-29 17:45:43 -0600

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,25 @@
+def add(p,q):
+    return p + q
+def subtract(p,q):
+    return p - q
+def multiply(p,q):
+    return p * q
+def divide(p,q):
+    return p / q
+print("a.add")
+print("b.subtract")
+print("c.multiply")
+print("d.divide")
+choice = input("enter choice(a/b/c/d):")
+num_1 = int(input("enter first number:"))
+num_2 = int(input("enter second number:"))
+if choice  == "a":
+    print (num_1,"+",num_2,"=",add(num_1,num_2))
+elif choice == "b":
+    print (num_1,"-",num_2,"=",subtract(num_1,num_2))
+elif choice == "c":
+    print (num_1,"*",num_2,"=",multiply(num_1,num_2))
+elif choice == "d":
+    print (num_1,"/",num_2,"=",divide(num_1,num_2))
+else:
+    print("invalid input")
